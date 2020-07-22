f987269af45668bb7c82eb96c1a129eed19cd2cf
Crew Spence <csspence123@gmail.com>
2020-07-22 05:46:48 -0500

    Created function stub for toy problem.

---
+++ binary_search_udemy.js
@@ -0,0 +1,10 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass - Udemy
+
+binarySearch
+Write a function binarySearch that accepts a sorted array and a value.  Create a left pointer at the start of the array, and a right pointer at the end of the array.  While the left pointer comes before the right pointer: create a pointer in the middle, if you find the value you want, return the index, if the value is too small, move the pointer up, if the value is too large, move the pointer down.  If you never find the value, return -1.
+*/
+
+const binarySearch = (arr, val) => {
+  
+}
